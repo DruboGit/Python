7f94a875d4be728aa100eb9feb3be52ce2544f8f
Diezel Löfgren <dilo1206@storsjogymnasiet.se>
2023-09-01 13:57:18 +0200

    made funny hrony thing

---
+++ First lesson things/horny.py
@@ -0,0 +1,8 @@
+horny = input("Horny? y/n\n")
+if horny == "y":
+    print("Bonk! Go horny jail!")
+elif horny == "n":
+    print("Good! :D")
+else:
+    print("That is not a viable answer!")
+input()
