78f987d6e01c666425eede3860b1d2fa5177c794
Diezel Löfgren <dilo1206@storsjogymnasiet.se>
2023-09-11 17:58:10 +0200

    made funny rickroll thing

---
+++ Random stuff/funmaybe.py
@@ -0,0 +1,14 @@
+import pyautogui as auto
+import time as t
+rickroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+for i in range(50):
+    auto.press("volumeup")
+auto.press("win")
+t.sleep(0.2)
+auto.typewrite("google chrome")
+auto.press("enter")
+t.sleep(0.2)
+auto.typewrite(rickroll)
+auto.press("enter")
+t.sleep(3)
+auto.press("f")
