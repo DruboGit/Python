26a7523cd4a4b1dd1bf89a2c92707f8a079bbb0c
Diezel Löfgren <dilo1206@storsjogymnasiet.se>
2024-01-19 12:39:43 +0100

    General changes to stuff

---
+++ Random stuff/Aim trainer cheat.py
@@ -0,0 +1,40 @@
+import pyautogui
+import numpy as np
+import time
+
+def detect_color(color, region):
+    # Get the screen image
+    screenshot = pyautogui.screenshot(region=region)
+    
+    # Convert the image to a numpy array
+    image_array = np.array(screenshot)
+    
+    # Find the coordinates where the color is present
+    color_coordinates = np.where(np.all(image_array == color, axis=-1))
+    
+    return color_coordinates
+
+# Define the RGB values of the color to be detected
+color_to_detect = (149, 195, 232)
+
+# Define the region of the screen to search for the color
+region_to_search = (0, 0, 1920, 1080)  #top, left , width, height 
+
+input("Ready?")
+time.sleep(3)
+while True:
+    # Call the detect_color function
+    coordinates = detect_color(color_to_detect, region_to_search)
+
+    # Check if any coordinates are found
+    if len(coordinates[0]) > 0:
+        
+        # Zip the x and y coordinates together
+        coordinate_pairs = list(zip(coordinates[1], coordinates[0]))
+        
+        # Print the coordinates in a user-friendly format
+        for coordinate in coordinate_pairs:
+            #input(f"Color {color_to_detect} found at coordinates: {coordinate}")
+            pyautogui.moveTo(coordinate)
+    else:
+        print(f"No color {color_to_detect} found on the screen.")
--- Random stuff/sight.py
+++ Random stuff/sight.py
@@ -14,7 +14,7 @@ def detect_color(color, region):
     return color_coordinates
 
 # Define the RGB values of the color to be detected
-color_to_detect = (255, 0, 0)
+color_to_detect = (149, 195, 232)
 
 # Define the region of the screen to search for the color
 region_to_search = (0, 0, 1920, 1080)  #top, left , width, height 
@@ -24,12 +24,13 @@ coordinates = detect_color(color_to_detect, region_to_search)
 
 # Check if any coordinates are found
 if len(coordinates[0]) > 0:
+    
     # Zip the x and y coordinates together
     coordinate_pairs = list(zip(coordinates[1], coordinates[0]))
     
     # Print the coordinates in a user-friendly format
     for coordinate in coordinate_pairs:
-        input(f"Color {color_to_detect} found at coordinates: {coordinate}")
+        #input(f"Color {color_to_detect} found at coordinates: {coordinate}")
         pyautogui.moveTo(coordinate)
 else:
-    input(f"No color {color_to_detect} found on the screen.")
+    print(f"No color {color_to_detect} found on the screen.")
