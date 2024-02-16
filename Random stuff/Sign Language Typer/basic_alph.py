56dc1ae94173bf9dca1acad2e4a82c0ff35b0c34
Diezel Löfgren <dilo1206@storsjogymnasiet.se>
2024-02-16 15:07:43 +0100

    Started with sign language thing

--- Random stuff/Sign Language Typer/basic_alph.py
+++ Random stuff/Sign Language Typer/basic_alph.py
@@ -1,3 +1,14 @@
+from webcam import Webcam
+import cv2
+
+webcam = Webcam(src=0, w=640)
+print (f"Frame size: {webcam.w} x {webcam.h}")
+
+for frame in webcam:
+    cv2.imshow('Webcam Frame', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 #Take screenshots
 #Look for hands
 #Interperate sign
